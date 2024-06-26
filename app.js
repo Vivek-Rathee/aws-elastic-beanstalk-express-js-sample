3dafea6d326803dbaff30092d2005959ac3dbe0e
Rathee <iamvivekrathee@gmail.com>
2024-06-26 05:02:16 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Something other than Hello World!'));
+app.get('/', (req, res) => res.send('This Continuous Delivery Pipeline has a review stage requiring manual approval.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
