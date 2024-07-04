932eaf3f70728952161a9d9fb1d5f76650bad439
Vivek Rathee <143781362+Vivek-Rathee@users.noreply.github.com>
2024-07-04 18:09:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This Continuous Delivery Pipeline has a review stage requiring manual approval.'));
+app.get('/', (req, res) => res.send('This Continuous Delivery Pipeline has a review stage requiring manual approval. If you do not see your changes, please wait for approval.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
